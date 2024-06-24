ede1848bc3b9816182e8b9bdcb0d1bba5ca5a113
Ayush Kushwaha <111639899+akayushk9648@users.noreply.github.com>
2024-06-24 23:09:20 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,10 @@
-echo " BUILD START"
+# Build the project
+echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput -clear
-echo " BUILD END"
+
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
