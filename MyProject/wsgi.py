fec961877635c3bcaf8553e686e3761323469a33
Ayush Kushwaha <111639899+akayushk9648@users.noreply.github.com>
2024-07-17 16:28:08 +0530

    Update wsgi.py

--- MyProject/wsgi.py
+++ MyProject/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for deploy_on_pythonanywhere project.
+WSGI config for MyProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.8/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deploy_on_pythonanywhere.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyProject.settings')
 
 application = get_wsgi_application()
